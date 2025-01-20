d192a581e84490bcda959e1c11362430362f2b29
Benjamin Bugl <benjamin.bugl@gmx.net>
2025-01-20 11:28:11 +0100

    vscode: 1.96.2 -> 1.96.4

--- pkgs/applications/editors/vscode/vscode.nix
+++ pkgs/applications/editors/vscode/vscode.nix
@@ -47,7 +47,7 @@ in
 callPackage ./generic.nix rec {
   # Please backport all compatible updates to the stable release.
   # This is important for the extension ecosystem.
-  version = "1.96.2";
+  version = "1.96.4";
   pname = "vscode" + lib.optionalString isInsiders "-insiders";
 
   # This is used for VS Code - Remote SSH test
